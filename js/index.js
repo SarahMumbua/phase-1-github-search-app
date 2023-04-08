2eb069dc9967b6c3aa574c9f5057bc79085f7a84
SarahMumbua <mumbuasarah2030@gmail.com>
2023-04-08 13:06:49 +0300

    added displayUsers function

--- js/index.js
+++ js/index.js
@@ -10,9 +10,34 @@ form.addEventListener('submit', (event) => {
     fetch(uri)
         .then(response => response.json())
         .then(data => {
+            console.log(data);
+            console.log(data.items);
             displayUsers(data.items)
         })
         .catch(error => {
             console.log(error);
         })
 });
+//display search results
+function displayUsers(users){
+    userList.innerHTML ='';
+    users.forEach(user => {
+        const li = document.createElement("li");
+        const userName =document.createElement("span");
+        const avatar = document.createElement("img");
+        const profileLink = document.createElement("a");
+        userName.innerText = user.login;
+        avatar.src = user.avatar_url;
+        profileLink.href =user.html_url;
+        profileLink.innerText ="view Profile";
+
+        li.appendChild(userName);
+        li.appendChild(avatar);
+        li.appendChild(profileLink);
+        userList.appendChild(li);
+
+        li.addEventListener('click',()=>{
+          displayRepos(user.repos_url)
+        })
+    });
+}
